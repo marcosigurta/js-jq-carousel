7f772165ec4790b81f99c0d1e5bdb06259ffd376
marco <marcosigurta.ovb@gmail.com>
2020-07-03 15:19:21 +0200

    Added @Media on small Device and Js. Js need to be fixed.

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $('.next').click(nextImg);
+    $('.prev').click(previousImg);
+});
+
+function nextImg() {
+    var image = $('img.active');
+    var icon = $('i.active');
+
+    image.removeClass('active');
+    icon.removeClass('active');
+
+    if (image.hasClass('last')) {
+        $('img.first').addClass('active');
+        $('i.first').addClass('active');
+    } else {
+        image.next().addClass('active');
+        icon.next().addClass('active');
+    }
+}
+
+function previousImg() {
+    var image = $('img.active');
+    var icon = $('i.active');
+
+    if (image.hasClass('first')) {
+        $('img.last').addClass('active');
+        $('i.last').addClass('active');
+    } else {
+        image.prev().addClass('active');
+        icon.prev().addClass('active');
+    }
+}
+
